a3869b6253cf3ad970d70d35dbb7f9957dde964f
K0NSTANT1N3 <programming255.konst.end.01@gmail.com>
2022-07-27 17:01:06 +0400

    Custom hooks

--- src/main/react-resources/src/App.js
+++ src/main/react-resources/src/App.js
@@ -29,6 +29,15 @@ const Search = ({search, onSearch}) =>
         <hr/>
     </>
 
+const useSemiPersistentState = (key, initialState) => {
+    const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+
+    React.useEffect(() => {
+        localStorage.setItem(key, value)
+    }, [value, key]);
+
+    return [value, setValue];
+};
 
 const App = () => {
     const stories = [
@@ -50,11 +59,7 @@ const App = () => {
         },
     ];
 
-    const [searchTerm, setSearchTerm] = React.useState('React');
-
-    React.useEffect(() => {
-        localStorage.setItem('search', searchTerm)
-    }, [setSearchTerm])
+    const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
     const handleSearch = event => {
         setSearchTerm(event.target.value);
